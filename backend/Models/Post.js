297442f02a574ddf999b224dd227458a38f79a4e
Navonil Das <navneeladas@gmail.com>
2020-12-15 18:01:21 +0530

    Post Model

---
+++ backend/Models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const Post = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    slug: { type: String, required: true, unique: true, index: true },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    posted_at: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('Post', Post);
