4a629d0656727fbe4f808882c5b57d5fbe3b756b
Navonil Das <navneeladas@gmail.com>
2020-12-17 04:06:26 +0530

    Routes and Global Styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import ViewPost from './components/ViewPost';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/create/post" exact component={CreatePost} />
+          <Route path="/edit/:id" exact component={CreatePost} />
+          <Route path="/read/:slug" exact component={ViewPost} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
