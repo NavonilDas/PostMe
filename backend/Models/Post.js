1ad0b2899a300063415bc7b87a4004edd6859f08
Navonil Das <navneeladas@gmail.com>
2020-12-26 09:20:21 +0530

    Modification on Post Model For Likes

--- backend/Models/Post.js
+++ backend/Models/Post.js
@@ -6,7 +6,8 @@ const Post = new mongoose.Schema({
     content: { type: String, required: true },
     slug: { type: String, required: true, unique: true, index: true },
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    posted_at: { type: Date, required: true }
+    posted_at: { type: Date, required: true },
+    likes: { type: Number, default: 0, required: true }
 });
 
 module.exports = mongoose.model('Post', Post);
