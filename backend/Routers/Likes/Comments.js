a62e6509535b88115b0c88bfd4ae94fa02a5f761
Navonil Das <navneeladas@gmail.com>
2020-12-26 12:33:13 +0530

    API route For Updating Like in Comment

--- backend/Routers/Likes/Comments.js
+++ backend/Routers/Likes/Comments.js
@@ -83,4 +83,48 @@ router.delete('/:comment_id', JWT, (req, res) => {
 });
 
 
+// Update Comment Like
+router.put('/:comment_id/:isUp', JWT, (req, res) => {
+    let { comment_id, isUp } = req.params;
+
+    try {
+        comment_id = mongoose.Types.ObjectId(comment_id);
+    } catch (_) {
+        return res.status(400).json({ error: 'Invalid Comment ID' });
+    }
+
+    const value = (isUp === 'up') ? 1 : -1;
+    const $inc = {
+        likes: 0
+    };
+    if (value === 1) $inc.likes = 2;
+    else $inc.likes = -2;
+
+    const find = {
+        comment_id,
+        user_id: new mongoose.Types.ObjectId(req.user._id),
+    };
+    const conf = { useFindAndModify: false };
+    // console.log($inc);
+
+    LikesSchema.findOneAndUpdate(find, { value }, conf, (err, doc) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Database Error' });
+        }
+        // if prev is not same as update
+        if (doc.value !== value) {
+            CommentSchema.findByIdAndUpdate(comment_id, { $inc }, conf, (error, _) => {
+                if (error) {
+                    console.error(error);
+                    return res.status(500).json({ error: 'Database Error' });
+                }
+                res.json({ status: 'Done' });
+            });
+        } else res.status(400).json({ error: 'Already Liked' });
+    });
+
+});
+
+
 module.exports = router;
